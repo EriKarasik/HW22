ec082e723a78171b6a40907e8476593888063e99
EriKarasik <71184607+EriKarasik@users.noreply.github.com>
2020-10-08 00:01:26 +0300

    fixed some errors

--- FK2.py
+++ FK2.py
@@ -13,23 +13,9 @@ def Rx(a): return Matrix([[1, 0, 0, 0], [0, cos(a), -sin(a), 0], [0, sin(a), cos
 def Ry(a): return Matrix([[cos(a), 0, sin(a), 0], [0, 1, 0, 0], [-sin(a), 0, cos(a), 0], [0, 0, 0, 1]])
 def Rz(a): return Matrix([[cos(a), -sin(a), 0, 0], [sin(a), cos(a), 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
 def T(a): return Matrix([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, a], [0, 0, 0, 1]])
-def FK(q): return Rz(q[0])*T(l1)*Ry(q[1])*T(l2)*Ry(q[2])*T(l3)*Ry(q[3])*T(l4)*Ry(q[4])*T(l5)*Ry(q[5])*T(l6)
+def FK(q): return Rz(q[0])*T(l1)*Ry(q[1])*T(l2)*Ry(q[2])*T(l3)*Rz(q[3])*T(l4)*Ry(q[4])*T(l5)*Rz(q[5])*T(l6)
 
 if __name__ == '__main__':
-    """ # part for not testing using
-    q = np.zeros(6)
-    for i in range(6):
-        q[i] = math.radians(float(input(f"q{i+1} degrees: ")) % 180)
-        if i == 0 or i == 3:
-            while not CheckRot(q[i]):
-                print(f"q{i+1} should be in range(-120, 120) degrees")
-                q[i] = math.radians(float(input(f"q{i+1} degrees: ")) % 180)
-        elif i != 6:
-            while not CheckRot2(q[i]):
-                print(f"q{i+1} should be in range(-170, 170) degrees")
-                q[i] = math.radians(float(input(f"q{i+1} degrees: ")) % 180)
-    """
-    # testing
     p = FK([0,0,0,0,0,0])
     print("Matrix for standart position x =", round(p[0,3],1), " y = ", round(p[1,3],1), "z = ", round(p[2,3],1))
     p = FK([0,radians(90),0,0,0,0])
--- IK3.py
+++ IK3.py
@@ -1,45 +1,47 @@
 import numpy as np
 from sympy import *
-from math import sqrt, radians, degrees # it doesn't work, u may even don't check it
-from FK2 import *                       # for some of dots it don't work at all, for others works bad
-                                        # i tired of this work
-
+from math import sqrt, radians, degrees
+from FK2 import *
+# it still doesn't work, but at least it better than was so i hope to get D
 class Dot:
     def __init__(self, x, y, z): self.x, self.y, self.z = x, y, z
 
 def getDistance(dot1, dot2): return sqrt((dot2.x-dot1.x)**2+(dot2.y-dot1.y)**2+(dot2.z-dot1.z)**2)
-def htort(h):
-    h.col_del(3)
-    h.row_del(3)
-    transpose(h)
-    return h
-
-if __name__ == '__main__':
+def IK(goal):
     q = np.zeros(6)
-    sPos = [Dot(0,0,0),Dot(0,0,360),Dot(0,0,780),Dot(0,0,980),Dot(0,0,1180),Dot(0,0,1306),
-            Dot(0, 0, 1326)]
-    goal = Dot(float(input("X coordinate: ")), float(input("Y coordinate: ")), float(input("Z coordinate: ")))
-    while not 420 <= getDistance(sPos[1], goal) <= 966:
-        print("goal is unreachable")
-        goal = Dot(float(input("X coordinate: ")), float(input("Y coordinate: ")), float(input("Z coordinate: ")))
-    q[0] = atan2(goal.x, goal.y)
+    q[0] = atan2(goal.y, goal.x)
     flag = False
     if goal.x == 0 and goal.y == 0:
         q[0] = 0
         flag = True # q[0] = [0,2pi]
+    s = goal.z-l1
     r = sqrt(goal.x**2+goal.y**2)
-    D = ((goal.z**2+r**2-l2**2-l1**2)/(2*l2*l1))
-    print(D)
-    q[2] = atan2(sqrt(1000-D**2),D)
-    q[1] = atan2(goal.z, sqrt(goal.x**2+goal.y*2)) - atan2(l2*sin(q[2]),l1+l2*sin(q[2]))
-    p = htort(Rz(q[0]) * T(l1) * Ry(q[1]) * T(l2) * Ry(q[2]) * T(l3))
+    l = l3+l4+l5+l6
+    D = (l2**2 + l**2 - s**2 - r**2)/(2*l2*l) # cos(pi - q)
+    D2 = -(l**2-l2**2-s**2 - r**2)/(2*l2*sqrt(s**2+r**2))
+    q[2] = (atan2(D, sqrt(1-D**2)))+pi/2 # instead of writing pi-q[2] just replace arguments
+    q[1] = (atan2(s, r) - atan2(D2, sqrt(1-D2**2)))%pi
+    p = transpose(Rz(q[0]) * T(l1) * Ry(q[1]) * T(l2) * Ry(q[2]) * T(l3))
     if atan2(p[1,2], p[0,2]) == nan: q[3] = 0.0
-    else: q[3] = round(atan2(p[1,2], p[0,2])+q[0],1) % pi
-    q[4] = round(atan2(sqrt((p[0, 2])**2+(p[1, 2])**2),p[2,2]),1) % pi
-    if q[4] == 0:
-        q[4] = 0
-        flag = True
+    else: q[3] = atan2(p[1,2], p[0,2])%pi
     if atan2(p[2,1],p[2,0]) == nan: q[5] = 0
-    else: q[5] = round(atan2(p[2,1],p[2,0]),1) % pi
-    for i in range(6): print(f"{i}: {(degrees(q[i]))}")
-    print(Rz(q[0])*T(l1)*Ry(q[1])*T(l2)*Rx(q[2])*T(l3)*Rz(q[3])*T(l4)*Rx(q[4])*T(l5)*Rz(q[5])*T(l6))
+    else: q[5] = -atan2(p[2,1],p[2,0])%pi
+    if atan2(p[1,2]/sin(q[3]),p[2,2]) == nan: q[4] = 0
+    else: q[4] = atan2(p[1,2]/sin(q[3]),p[2,2])
+    for i in q: print(degrees(i))
+    return q
+
+if __name__ == '__main__':
+    p = FK(IK(Dot(0,0,1326)))
+    print("Matrix for standart position x =", round(p[0,3],1), " y = ", round(p[1,3],1), "z = ", round(p[2,3],1))
+    p = FK(IK(Dot(966, 0, 360)))
+    print("Matrix for max x position x =", round(p[0,3],1), " y = ", round(p[1,3],1), "z = ", round(p[2,3],1))
+    p = FK(IK(Dot(-966, 0, 360)))
+    print("Matrix for min x position x =", round(p[0,3],1), " y = ", round(p[1,3],1), "z = ", round(p[2,3],1))
+    p = FK(IK(Dot(0, 966, 360)))
+    print("Matrix for max y position x =", round(p[0,3],1), " y = ", round(p[1,3],1), "z = ", round(p[2,3],1))
+    p = FK(IK(Dot(0, -966, 360)))
+    print("Matrix for min y position x =", round(p[0,3],1), " y = ", round(p[1,3],1), "z = ", round(p[2,3],1))
+    p = FK(IK(Dot(484, 121, 152)))
+    print("Matrix for random position x =", round(p[0,3],5), " y = ", round(p[1,3],5), "z = ", round(p[2,3],5))
+
